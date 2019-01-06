88665aa1be285dc840fc794622e82bdf6a7f8e2a
Sam Soheylee Arasi <ssoheyleearasi@gmail.com>
2019-01-06 07:21:15 +0000

    completions of javascripting tutorials

---
+++ iceCreamFlavours.js
@@ -0,0 +1,14 @@
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+var first = iceCreamFlavours[0];
+console.log(first);
+
+var last = iceCreamFlavours[iceCreamFlavours.length - 1];
+console.log(last);
+
+var size = iceCreamFlavours.length;
+console.log(size);
